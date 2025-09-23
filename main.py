78f69e17e1e65f773e738f2fcb6ea8b9f1cb5981
wwwenhx <userEmail>
2025-09-23 11:45:48 +0800

    fix

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 # 导入langchain包
 from langchain import chat_models
-from langchain_core.output_parsers import ResponseSchema, StructuredOutputParser
+from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import ChatPromptTemplate
 from config import DEPLOYMENT_NAME
 
 model = chat_models.init_chat_model(model=DEPLOYMENT_NAME, model_provider="azure_openai")
-parser = StructuredOutputParser.from_response_schemas
+parser = StrOutputParser()
 prompt_template = ChatPromptTemplate([
-    ("system", "你是主要学习langchain的高级agent工程师, 请根据你的学生提出的问题 做出解答"),
-    ("user","这是你的学生的问题: {topic}, 请详细解答学生的问题")
+    ("system", "帮我根据用户的语句 推断出他的年龄以及姓名"),
+    ("user","这是说的话 : {topic}")
 ])
 basic_qa_chain  = prompt_template | model | parser
-question = "我要从零开始学习agent 并且使用langchain 你会怎么教我 我应该去学习什么技术栈"
+question = "我叫王力宏,我今年39岁了"
+
 res = basic_qa_chain.invoke(question)
 print(res)
